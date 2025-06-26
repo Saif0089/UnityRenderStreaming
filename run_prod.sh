b3232bd2d4d493a12bad816e92b0aeada7bb7998
Ehtisham <67787465+Saif0089@users.noreply.github.com>
2025-06-26 15:52:45 -0700

    Add prod and staging deployment workflows and services
    Introduced GitHub Actions workflows, run scripts, and systemd service files for production and staging environments. Updated develop workflow, script, and service to use environment-specific directories. This enables automated deployment and management for develop, staging, and production branches.

--- run_prod.sh
+++ run_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PATH=/home/administrator/.nvm/versions/node/v22.17.0/bin:$PATH
-cd /home/administrator/develop/UnityRenderStreaming/WebApp
+cd /home/administrator/prod/UnityRenderStreaming/WebApp
 npm install --legacy-peer-deps
-npm run dev -- -p 5054
+npm run dev -- -p 8080
