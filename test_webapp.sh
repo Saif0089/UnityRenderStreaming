b4688b75ed22c45e7f6aa5199311d70b8d128bcb
Ehtisham <67787465+Saif0089@users.noreply.github.com>
2025-06-26 14:51:54 -0700

    Update CI to run test_webapp.sh and simplify script
    The GitHub Actions workflow now makes test_webapp.sh executable and runs it with the service port. The test_webapp.sh script was simplified by removing unused NODE_PATH logic and related PATH export.

--- test_webapp.sh
+++ test_webapp.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 SERVICE_PORT=${1:-8080} # Default to 8080 if no argument is provided
-NODE_PATH=${2:-$(which node)} # Default to `which node` if no argument is provided
-
-export PATH=$NODE_PATH:$PATH
 
 cd WebApp
 npm install --legacy-peer-deps
