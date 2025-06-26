652e799da01086b4d6b0b1f0e3a6043ad23630e8
Ehtisham <67787465+Saif0089@users.noreply.github.com>
2025-06-26 11:53:24 -0700

    Update test script and workflow for web app
    Refines the GitHub Actions workflow to only chmod shell files and simplifies test_webapp.sh by removing comments, hardcoding port 8080, and enabling lint, test, and newman steps.

--- test_webapp.sh
+++ test_webapp.sh
@@ -1,19 +1,7 @@
-#!/bin/bash -eu
-
-# Example usage:
-# To run the script with a specific port (e.g., 3000), use the following command:
-# ./test_webapp.sh 3000
-PORT=${1:-8080} # Use the first argument as the port, default to 8080 if not provided
-
-./install_npm.sh
-
-which npm
-which node
-
 cd WebApp
 npm install --legacy-peer-deps
-# npm run lint
-# npm run test
-npm run dev -- -p "$PORT" &
-# sleep 5
-# npm run newman -- -e ./test/env_macos.postman_environment.json
+npm run lint
+npm run test
+npm run dev -- -p 8080 &
+sleep 5
+npm run newman -- -e ./test/env_macos.postman_environment.json
