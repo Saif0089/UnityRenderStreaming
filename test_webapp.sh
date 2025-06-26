5b727101952075285cc8e6cae38f0bf63a4825f5
Ehtisham <67787465+Saif0089@users.noreply.github.com>
2025-06-26 17:15:27 +0500

    Add script execution step to GitHub Actions and parameterize port in test_webapp.sh

--- test_webapp.sh
+++ test_webapp.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -eu
 
+# Example usage:
+# To run the script with a specific port (e.g., 3000), use the following command:
+# ./test_webapp.sh 3000
+PORT=${1:-8080} # Use the first argument as the port, default to 8080 if not provided
+
+./install_npm.sh
+
 cd WebApp
 npm install --legacy-peer-deps
 npm run lint
 npm run test
-npm run dev -- -p 8080 &
+npm run dev -- -p "$PORT" &
 sleep 5
 npm run newman -- -e ./test/env_macos.postman_environment.json
