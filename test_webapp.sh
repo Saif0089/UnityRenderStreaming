c328345ad4f5807a7b13f7b0a6cd76b3964bd69d
Ehtisham <67787465+Saif0089@users.noreply.github.com>
2025-06-26 11:29:02 -0700

    Update test_webapp.sh

--- test_webapp.sh
+++ test_webapp.sh
@@ -5,10 +5,13 @@
 # ./test_webapp.sh 3000
 PORT=${1:-8080} # Use the first argument as the port, default to 8080 if not provided
 
+which npm
+which node
+
 cd WebApp
-/home/administrator/.nvm/versions/node/v22.17.0/bin/npm install --legacy-peer-deps
+npm install --legacy-peer-deps
 # npm run lint
 # npm run test
-/home/administrator/.nvm/versions/node/v22.17.0/bin/npm run dev -- -p "$PORT" &
+npm run dev -- -p "$PORT" &
 # sleep 5
 # npm run newman -- -e ./test/env_macos.postman_environment.json
