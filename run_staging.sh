5c4effadb8055b0fb0c8ee2fdc55fdc0a0d90cbd
Ehtisham <67787465+Saif0089@users.noreply.github.com>
2025-06-26 18:21:16 -0700

    Add npm install before legacy-peer-deps in run scripts
    Updated run_develop.sh, run_local.sh, run_prod.sh, and run_staging.sh to include 'npm install' before running 'npm install --legacy-peer-deps' in both client and root WebApp directories. This ensures all dependencies are installed properly before handling legacy peer dependencies.

--- run_staging.sh
+++ run_staging.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 export PATH=/home/administrator/.nvm/versions/node/v22.17.0/bin:$PATH
 cd /home/administrator/staging/UnityRenderStreaming/WebApp/client
+npm install
 npm install --legacy-peer-deps
 cd /home/administrator/staging/UnityRenderStreaming/WebApp
+npm install
 npm install --legacy-peer-deps
 npm run dev -- -p 5056
