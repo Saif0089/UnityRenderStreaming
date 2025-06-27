70f7ceeb46e17cdf9cfd2ef6b300b7198b4831a6
Ehtisham <67787465+Saif0089@users.noreply.github.com>
2025-06-26 17:57:19 -0700

    Remove legacy peer deps flag from client npm install
    The npm install command in the client directory no longer uses the --legacy-peer-deps flag, aligning it with standard dependency installation practices.

--- run_develop.sh
+++ run_develop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/home/administrator/.nvm/versions/node/v22.17.0/bin:$PATH
 cd /home/administrator/develop/UnityRenderStreaming/WebApp/client
-npm install --legacy-peer-deps
+npm install
 cd /home/administrator/develop/UnityRenderStreaming/WebApp
 npm install --legacy-peer-deps
 npm run dev -- -p 5054
